6da8e07a7d44e813ac62801275a82e31c538a4f8
dev99pedro <somdomarx10@gmail.com>
2024-12-29 11:41:13 -0300

    att

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,35 @@
+require = 'pry'
+require 'net/http'
+
+class HomeController < ApplicationController
+
+
+    CURRENCIES =[
+      {code: 'USD-BRL'},
+      {code: 'EUR-BRL'},
+      {code: 'BTC-BRL'}
+    ]
+
+    def index
+        @chart_data = []
+
+       
+        CURRENCIES.each do |currency|
+        url= URI("https://economia.awesomeapi.com.br/json/daily/#{currency[:code]}/30")
+        response = Net::HTTP.get(url)
+        data = JSON.parse(response)
+
+
+        hash = {}
+        data.each do | entry |
+          date =  Time.at(entry['timestamp'].to_i).strftime("%d/%m/%Y")
+          rate = entry['high']
+         
+          hash[date] = rate  
+        end
+
+        @chart_data << { name:currency[:code], data:hash}
+      end
+
+    end     
+end
